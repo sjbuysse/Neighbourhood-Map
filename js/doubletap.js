5a7da8d32c32ab6ff646bd2c11961e076cedc2c4
Simon Buysse <buysse.simon@gmail.com>
2017-02-23 18:34:17 +1300

    Add doubleTap event functionality

---
+++ js/doubletap.js
@@ -0,0 +1,26 @@
+// Create custom event binding for listening to double taps on mobile devices
+ko.bindingHandlers.doubleTap = {
+    init: function(element, valueAccessor, allBindingsAccessor, data, bindingContext){
+        var tappedTwice = false;
+        var wrappedHandler, newValueAccessor;
+
+        // wrap the handler with a check for the enter key
+        wrappedHandler = function (data, event) {
+            if (!tappedTwice) {
+                tappedTwice = true;
+                setTimeout( function() { tappedTwice = false;  }, 300  );
+                return false;
+            }
+            valueAccessor().call(this, data, event);
+        };
+        // create a valueAccessor with the options that we would want to pass to the event binding
+        newValueAccessor = function () {
+            return {
+                click: wrappedHandler
+            };
+        };
+
+        // call the real event binding's init function
+        ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
+    }
+}
