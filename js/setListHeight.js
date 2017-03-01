7860aebe984a9d982ed0f120822de46381bb318b
Simon Buysse <buysse.simon@gmail.com>
2017-03-01 23:42:26 +1300

    Style large infowindow

--- js/setListHeight.js
+++ js/setListHeight.js
@@ -1,3 +1,4 @@
+//Source: http://stackoverflow.com/questions/3837037/div-with-scrollbar-inside-div-with-positionfixed
 (function($) {
   $.fn.getPotentialHeight = function() {
         var $element = this;
@@ -12,5 +13,6 @@ $(document).ready(
     function() {
         var potentialHeight = $('#filter-list').getPotentialHeight();
         $('#filter-list').css('height', potentialHeight);
+        $('#info-wrapper').css('height', potentialHeight);
     }
 );
