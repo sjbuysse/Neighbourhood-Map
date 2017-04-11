76a4673f949af856457a03ba15ddda1a885467d4
Simon Buysse <buysse.simon@gmail.com>
2017-04-11 23:51:39 +1200

    Add androidbug.js

---
+++ js/androidbug.js
@@ -0,0 +1,9 @@
+if(/Android [4-6]/.test(navigator.appVersion)) {
+   window.addEventListener("resize", function() {
+      if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+         window.setTimeout(function() {
+            document.activeElement.scrollIntoViewIfNeeded();
+         },0);
+      }
+   })
+}
