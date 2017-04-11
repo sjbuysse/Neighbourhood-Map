70489671c4892140fbd28cc44450aee0c2caf63a
Simon Buysse <buysse.simon@gmail.com>
2017-04-11 23:54:52 +1200

    Add log

--- js/androidbug.js
+++ js/androidbug.js
@@ -1,10 +1,8 @@
-if(/Android [4-6]/.test(navigator.appVersion)) {
-   window.addEventListener("resize", function() {
-      if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
-          console.log("bugggy");
-         window.setTimeout(function() {
-            document.activeElement.scrollIntoViewIfNeeded();
-         },0);
-      }
-   })
-}
+window.addEventListener("resize", function() {
+  if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+      console.log("bugggy");
+     window.setTimeout(function() {
+        document.activeElement.scrollIntoViewIfNeeded();
+     },0);
+  }
+})
