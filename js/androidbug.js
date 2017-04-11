4768a71bba8cc346de922977c84418c3bd919f09
Simon Buysse <buysse.simon@gmail.com>
2017-04-11 23:53:17 +1200

    Add log

--- js/androidbug.js
+++ js/androidbug.js
@@ -1,6 +1,7 @@
 if(/Android [4-6]/.test(navigator.appVersion)) {
    window.addEventListener("resize", function() {
       if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+          console.log("bugggy");
          window.setTimeout(function() {
             document.activeElement.scrollIntoViewIfNeeded();
          },0);
